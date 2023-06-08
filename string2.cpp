0b45abc70fc434357d62f197186c7516bc73bc4c
Gaurav1756 <gauravsenger2167@gmail.com>
2023-06-08 08:30:08 +0530

    second commit

--- string2.cpp
+++ string2.cpp
@@ -1,7 +1,11 @@
 #include<iostream>
 using namespace std;
 int main() {
-    string fullname;
-    cout<<"Enter your fullname : ";
-    cin>>fullname;
+    string firstname, lastname, fullname;
+    cout<<"Enter your firstname : ";
+    cin>>firstname;
+    cout<<"Enter your lastname : ";
+    cin>>lastname;
+    fullname = firstname + " " + lastname;
+    cout<<"Name : "<<fullname;
 }
