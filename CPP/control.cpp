a610ea97ccd69a8bdb251f6619d7872c037d06ea
Gaurav1756 <gauravsenger2167@gmail.com>
2023-06-10 00:18:52 +0530

    third

---
+++ CPP/control.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int num = 8;
+    string output = (num < 10) ? "less than 10 " : "greater than 10";
+    cout<<output;
+}
---
+++ CPP/loop1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() {
+    // for(int i = 1; i <= 3; i++) {
+    //     for(int j = 1; j <=3; j++) {
+    //         cout<<i<<" "<<j<<endl;
+    //     }
+    // }
+    // int i = 1;
+    // while(i <= 3) {
+    //     for(int j = 1; j <= 3; j++) {
+    //         cout<<i<<" "<<j<<endl;
+    //     }
+    // }
+    int a = 5;
+    do {
+        cout<<a<<" ";
+        a++;
+    }while(a < 5);
+}
