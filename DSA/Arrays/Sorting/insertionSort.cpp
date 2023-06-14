52974e4a12f51f06b9d62be5b114c3508f30b463
Gaurav1756 <gauravsenger2167@gmail.com>
2023-06-15 00:34:33 +0530

    new

---
+++ DSA/Arrays/Sorting/insertionSort.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[], int size) {
+    
+}
+
+int main() {
+    int arr[] = {5,6,3,1,2,8,7};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    insertionSort(arr,size);
+}
