4e76ae3e46d93b2b54146552fcb45757db61ce5b
Gaurav1756 <gauravsenger2167@gmail.com>
2023-06-26 11:52:44 +0530

    nineteen

---
+++ DSA/LinkedList/questions.cpp
@@ -0,0 +1,10 @@
+//Remove duplicates from a sorted Linked List
+//Remove duplicates from unsorted linked list
+//Starting Node of loop in Linked List
+//Remove Loop from Linked List
+//Merge two sorted linked lists
+// 012 sort
+//check Palindrome in linked list
+//Add two numbers represented by Linked List
+//Merge Sort in Linked List
+//Clone a Linked List with random pointers
---
+++ DSA/Stack/intro2.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+class Stack {
+    int *arr;
+    int top;
+    int size;
+    public:
+    Stack() {
+        arr = new int[size];
+        top = -1;
+    }
+    void push(int val) {
+        if(top == size-1) {
+            cout<<"Stack overflow"<<endl;
+            return;
+        }
+        top++;
+        arr[top] = val;
+    }
+    void pop() {
+        if(top == -1) {
+            cout<<"No elements to pop"<<endl;
+            return;
+        }
+        top--;
+    }
+    int Top() {
+        if(top == -1) {
+            cout<<"No elements in stack"<<endl;
+            return -1;
+        }
+        return arr[top];
+    }
+    bool empty() {
+        return top == -1;
+    }
+};
+
+int main() {
+    Stack s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    cout<<s.Top()<<endl;
+}
